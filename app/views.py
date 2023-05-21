090eefcdc1c2caa1330bbe8b97a26b700203c4a7
emresin <Emresin75@outlook.com>
2023-05-21 17:08:43 +0300

    login system being implemented

--- app/views.py
+++ app/views.py
@@ -1,5 +1,23 @@
 from dotenv import load_dotenv
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
+
+
+
+login_manager = LoginManager()
+migrate = Migrate()
+bcrypt = Bcrypt()
+
 
 from app.crud_table import crud_table_blueprint
 
@@ -7,6 +25,10 @@ load_dotenv()
 
 app = Flask(__name__)
 
+#initialize for the login system
+login_manager.init_app(app)
+migrate.init_app(app, db)
+bcrypt.init_app(app)
 
 @app.route('/')
 def hello_world():
