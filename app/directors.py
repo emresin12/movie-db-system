5f1cd38c4201c706a93c71c58357c270a9d6e80e
emresin <Emresin75@outlook.com>
2023-05-23 21:27:30 +0300

    Revert "login system is fixed for blueprints"
    This reverts commit a72019c478892849c62a82e89e233e951322a43f.

--- app/directors.py
+++ app/directors.py
@@ -4,32 +4,13 @@
 
 from clients.postgres.postgresql_db import postgres_aws
 from flask import render_template, Blueprint
-from .views import current_user, app
-from functools import wraps
 
-director_blueprint = Blueprint("director_blueprint", __name__)
-
-
-def login_required(role="ANY"):
-    def wrapper(fn):
-        @wraps(fn)
-        def decorated_view(*args, **kwargs):
-            with app.app_context():  # Create a temporary application context
-                if not current_user.is_authenticated:
-                    return app.login_manager.unauthorized()
-                urole = current_user._get_current_object().get_urole()
-                print(urole)
-                if (urole != role) and (role != "ANY"):
-                    return render_template("index.html", error="not authorized")
-            return fn(*args, **kwargs)
-
-        return decorated_view
-
-    return wrapper
+director_blueprint = Blueprint(
+    "director_blueprint", __name__
+)
 
 
-@director_blueprint.route("/directors")
-@login_required("Database_Manager")
+@director_blueprint.route('/directors')
 def view_directors():
     query = """
     select d.username, u.name, u.surname, n.nation, dww.platform_id, rp.platform_name from directors d 
@@ -39,6 +20,4 @@ def view_directors():
     join ratingplatform rp on dww.platform_id = rp.platform_id
     """
     data = postgres_aws.get_rows_and_fields_from_sql(query)
-    return render_template(
-        "TableView.html", fields=data[0], rows=data[1], table_title="Directors"
-    )
+    return render_template('TableView.html', fields=data[0], rows=data[1], table_title='Directors')
--- app/movies.py
+++ app/movies.py
@@ -5,35 +5,15 @@
 
 from clients.postgres.postgresql_db import postgres_aws
 from flask import render_template, Blueprint
-from .views import current_user, app
-from functools import wraps
 
-movie_blueprint = Blueprint("director_blueprint", __name__)
-
-
-def login_required(role="ANY"):
-    def wrapper(fn):
-        @wraps(fn)
-        def decorated_view(*args, **kwargs):
-            with app.app_context():  # Create a temporary application context
-                if not current_user.is_authenticated:
-                    return app.login_manager.unauthorized()
-                urole = current_user._get_current_object().get_urole()
-                print(urole)
-                if (urole != role) and (role != "ANY"):
-                    return render_template("index.html", error="not authorized")
-            return fn(*args, **kwargs)
-
-        return decorated_view
-
-    return wrapper
+movie_blueprint = Blueprint(
+    "director_blueprint", __name__
+)
 
 
-@movie_blueprint.route("/movies")
+@movie_blueprint.route('/movies')
 def view_directors():
     query = """
     """
     data = postgres_aws.get_rows_and_fields_from_sql(query)
-    return render_template(
-        "TableView.html", fields=data[0], rows=data[1], table_title="Directors"
-    )
+    return render_template('TableView.html', fields=data[0], rows=data[1], table_title='Directors')
