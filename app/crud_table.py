a72019c478892849c62a82e89e233e951322a43f
emresin <Emresin75@outlook.com>
2023-05-23 21:11:50 +0300

    login system is fixed for blueprints

--- app/crud_table.py
+++ app/crud_table.py
@@ -1,5 +1,26 @@
 from clients.postgres.postgresql_db import postgres_aws
 from flask import render_template, Blueprint, request
+from .views import current_user, app
+from functools import wraps
+import os
+
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            with app.app_context():  # Create a temporary application context
+                if not current_user.is_authenticated:
+                    return app.login_manager.unauthorized()
+                urole = current_user._get_current_object().get_urole()
+                print(urole)
+                if (urole != role) and (role != "ANY"):
+                    return render_template("index.html", error="not authorized")
+            return fn(*args, **kwargs)
+
+        return decorated_view
+
+    return wrapper
 
 
 def get_table_fields(table_name):
@@ -8,17 +29,21 @@ def get_table_fields(table_name):
         FROM information_schema.columns
         WHERE table_name = '{}'
         order by ordinal_position asc
-    """.format(table_name)
+    """.format(
+        table_name
+    )
     response = postgres_aws.get(query)
     print(response)
-    return [column['column_name'] for column in response]
+    return [column["column_name"] for column in response]
 
 
 def get_table_data(table_name):
     query = """
         SELECT *
         FROM {}
-    """.format(table_name)
+    """.format(
+        table_name
+    )
     rows = postgres_aws.get_as_list(query)
     print(rows)
     return rows
@@ -30,26 +55,30 @@ def get_table_info(table_name):
     return fields, rows
 
 
-crud_table_blueprint = Blueprint(
-    "crud_table_blueprint", __name__
-)
+crud_table_blueprint = Blueprint("crud_table_blueprint", __name__)
 
 
-@crud_table_blueprint.route('/examplecrud')
+@crud_table_blueprint.route("/examplecrud")
+@login_required(role="Audience")
 def example1():
-    table_name = 'theatretest'
+    table_name = "theatretest"
     primary_key = "theatre_id"
     data = get_table_info(table_name)
-    return render_template('CrudTable.html', fields=data[0], rows=data[1], table_name=table_name,
-                           primary_key=primary_key)
+    return render_template(
+        "CrudTable.html",
+        fields=data[0],
+        rows=data[1],
+        table_name=table_name,
+        primary_key=primary_key,
+    )
 
 
-@crud_table_blueprint.route('/submit', methods=['POST'])
+@crud_table_blueprint.route("/submit", methods=["POST"])
 def submit():
     # Handle the form submission here
-    table_name = 'theatretest'
+    table_name = "theatretest"
     primary_key = "theatre_id"
-    if request.method=='POST':
+    if request.method == "POST":
         # Access the form data
         data = request.form
         for key, value in data.items():
@@ -68,21 +97,21 @@ def submit():
             return str(e)
 
 
-@crud_table_blueprint.route('/delete', methods=['POST'])
+@crud_table_blueprint.route("/delete", methods=["POST"])
 def delete():
     # Handle the form submission here
     table_name = "theatretest"
     primary_key = "theatre_id"
-    if request.method == 'POST':
+    if request.method == "POST":
         # Access the form data
         data = request.form
-        primary_key_value = data['primary_key_value']
+        primary_key_value = data["primary_key_value"]
         query = f"DELETE FROM {table_name} WHERE {primary_key} = '{primary_key_value}'"
         postgres_aws.write(query)
         return "Object is deleted successfully!"
 
 
-@crud_table_blueprint.route('/exampleview')
+@crud_table_blueprint.route("/exampleview")
 def example2():
     query = """select sub.username                          as username,
        sub.password                          as password,
@@ -97,4 +126,6 @@ from (select u.username, u.password
       select dm.username, dm.password
       from databasemanagers dm) sub"""
     data = postgres_aws.get_rows_and_fields_from_sql(query)
-    return render_template('TableView.html', fields=data[0], rows=data[1], table_title='All Users')
+    return render_template(
+        "TableView.html", fields=data[0], rows=data[1], table_title="All Users"
+    )
